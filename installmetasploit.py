3b0c1c3077ae02e36d615e50fb4b7a3a2f7d9471
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2020-09-11 15:42:28 -0400

    Update installmetasploit.py

--- installmetasploit.py
+++ installmetasploit.py
@@ -6,7 +6,7 @@ print("this script will install metasploit")
 os.system("figlet script by youssef")
 os.system("pkg update && pkg upgrade -y && pkg install wget curl openssh git -y && pkg install git -y")
 os.system("wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh && chmod +x metasploit.sh")
-os.system("./metasploit.sh && ./postgresql_ctl.sh start")
+os.system("bash metasploit.sh && bash postgresql_ctl.sh start")
 print("metasploit will be started")
 
 os.system("msfconsole")
