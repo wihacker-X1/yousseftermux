0d4638482900a85454aab4546714264700898f04
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2020-09-11 15:28:30 -0400

    Update installmetasploit.py

--- installmetasploit.py
+++ installmetasploit.py
@@ -5,9 +5,10 @@ print("this script will install metasploit")
 
 os.system("figlet script by youssef")
 os.system("pkg update && pkg upgrade -y && pkg install wget curl openssh git -y && pkg install git -y")
+os.system("cd /data/data/com.termux/files/home")
 os.system("git clone https://github.com/gushmazuko/metasploit_in_termux")
 os.system("cd metasploit_in_termux")
-os.system("chmod +x *")
+os.system("chmod 777 *")
 os.system("./metasploit.sh")
 os.system("./postgresql_ctl.sh start")
 
