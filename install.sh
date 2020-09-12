4981400e312af1d4c15b36a3a910e34fd76bd4fa
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2020-09-12 11:57:21 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrad
+
+pkg install python 
+
+pkg install wget
+
+pkg install figlet 
+
+figlet tool req installed 
+
+python  yousseftool.py
---
+++ update.sh
@@ -0,0 +1,13 @@
+echo removing the old script 
+
+rm -r yousseftool.py
+
+echo updating
+
+pkg install wget 
+
+wget https://raw.githubusercontent.com/youhacker55/yousseftermux/master/yousseftool.py
+
+echo updated
+
+figlet enjoy
