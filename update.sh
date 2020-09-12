912b45def8f8ee115c6f3f43f4738f5f74008289
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2020-09-12 06:05:38 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,14 @@
-echo removing the old script 
+echo removing the old script
+sleep 10s
 
 rm -r yousseftool.py
-
+sleep 10s
 echo updating
 
 pkg install wget 
 
 wget https://raw.githubusercontent.com/youhacker55/yousseftermux/master/yousseftool.py
-
+sleep 10s
 echo updated
 
 figlet enjoy
